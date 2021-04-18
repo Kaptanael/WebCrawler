8b7e72c3239432d043ce952cf7c597020fd8d4a4
Kaptanael <arzaman@ael-bd.com>
2021-04-18 15:28:38 +0600

    Add project files.

---
+++ WebCrawler.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly ILogger<ArticlesController> _logger;
+
+        public ArticlesController(ILogger<ArticlesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
---
+++ WebCrawler.Api/Models/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Api.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public bool Archived { get; set; }
+        public DateTime CrawledDate { get; set; }
+    }
+}
