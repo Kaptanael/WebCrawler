d123a67eca742cf1f79f0cf97d3459ac1daf9549
Kaptanael <arzaman@ael-bd.com>
2021-04-19 06:38:50 +0600

    insert articles into bing

--- WebCrawler.ConsoleApp/Program.cs
+++ WebCrawler.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace WebCrawler.ConsoleApp
@@ -12,10 +13,38 @@ namespace WebCrawler.ConsoleApp
         static async Task Main(string[] args)
         {
             Console.WriteLine("Enter Search Words: ");
+
             string inputString = Console.ReadLine();            
             string searchString = inputString.Replace(" ", "+");
+
             var bingArticlesToCreate = await GetBingArticlesToCreateAsync(searchString);
+            var bingResult = InsertArticle(bingArticlesToCreate);
+
+            if (bingResult) 
+            {
+                Console.WriteLine("Bing articles has been inserted successfully.");
+            }
+
             Console.ReadLine();
+        }
+
+        private static bool InsertArticle(List<ArticleForListDto> articles) 
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:50000/api/articles/");                
+                var postArticles = client.PostAsJsonAsync<List<ArticleForListDto>>("save-articles", articles);
+                postArticles.Wait();
+
+                var result = postArticles.Result;
+
+                if (result.IsSuccessStatusCode) 
+                {
+                    return true;
+                }
+
+                return false;
+            }
         }
 
         private static async Task<List<ArticleForListDto>> GetBingArticlesToCreateAsync(string searchString)
